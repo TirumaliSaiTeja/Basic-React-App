92e8f058349dcfc74b4e9e493815da43361f3b00
saiteja <tsaiteja7@gmail.com>
2020-05-07 01:50:32 +0530

    import and export properties

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Loading from "./Loading";
+
+//class components are called state full
 
 class App extends Component {
   constructor(props) {
@@ -26,16 +29,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {!this.state.loading
-          ? this.state.users.map(user => (
-              <div>
-                <h1>{user.name.first}</h1>
-                <h2>{user.cell}</h2>
-                <h3>{user.email}</h3>
-                <hr />
-              </div>
-            ))
-          : "Loading"}
+        {!this.state.loading ? (
+          this.state.users.map(user => (
+            <div>
+              <h1>{user.name.first}</h1>
+              <h2>{user.cell}</h2>
+              <h3>{user.email}</h3>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <Loading message="Wait to load" />
+        )}
       </div>
     );
   }
---
+++ src/Loading.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const Loading = ({ message }) => <h2>{message}</h2>;
+//if we have many components, then no need of using export default
+export default Loading;
